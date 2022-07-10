93bc8cd89009a7d621fa4d049fab845518e3ae3b
Vipul Jain <vipuljain3456@gmail.com>
2022-07-11 00:29:07 +0530

    Added new items in Monday Menu Branch

--- src/main/java/com/vipul/MondayMenu.java
+++ src/main/java/com/vipul/MondayMenu.java
@@ -3,5 +3,6 @@ package com.vipul;
 public class MondayMenu {
     public static void main(String[] args) {
         System.out.println("Monday Menu");
+        System.out.println("Added new items");
     }
 }
